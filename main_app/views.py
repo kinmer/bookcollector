cd6799bf3002f871a9bba32e85a0dcea813f924c
yongZhang <kinmer7@gmail.com>
2024-04-11 14:42:54 -0400

    homework part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
+books = [
+  {'title': 'The Sandman', 'author': 'Neil Gaiman', 'genre': 'Comic', 'publish_year': 1989},
+  {'title': 'American Gods', 'author': 'Neil Gaiman', 'genre': 'Fiction', 'publish_year': 2001},
+]
+
+
+
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def books_index(request):
+  return render(request, 'books/index.html', {
+    'books': books
+  })
