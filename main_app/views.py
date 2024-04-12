d54b7375857b6d90a7866b6c04da262af2700c00
yongZhang <kinmer7@gmail.com>
2024-04-12 15:37:54 -0400

    part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,7 @@
-books = [
-  {'title': 'The Sandman', 'author': 'Neil Gaiman', 'genre': 'Comic', 'publish_year': 1989},
-  {'title': 'American Gods', 'author': 'Neil Gaiman', 'genre': 'Fiction', 'publish_year': 2001},
-]
-
 
 
 from django.shortcuts import render
-
+from .models import Book
 def home(request):
   return render(request, 'home.html')
 
@@ -14,6 +9,13 @@ def about(request):
   return render(request, 'about.html')
 
 def books_index(request):
+  books = Book.objects.all()
   return render(request, 'books/index.html', {
     'books': books
   })
+
+def books_detail(request, book_id):
+  book = Book.objects.get(id=book_id)
+  return render(request, 'books/detail.html', { 'book': book})
+
+
