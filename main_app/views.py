7e2c76a3704e3447de6bf78100e49acf58c861ea
yongZhang <kinmer7@gmail.com>
2024-04-15 15:38:53 -0400

    part 3

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,34 @@
-
-
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Book
+
+
+
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
 
 def about(request):
-  return render(request, 'about.html')
+     return render(request, 'about.html')
 
 def books_index(request):
-  books = Book.objects.all()
-  return render(request, 'books/index.html', {
-    'books': books
-  })
+    books = Book.objects.all()
+    return render(request, 'books/index.html', {
+        'books': books
+    })
 
 def books_detail(request, book_id):
-  book = Book.objects.get(id=book_id)
-  return render(request, 'books/detail.html', { 'book': book})
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/detail.html', { 'book': book})
 
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+    # success_url = '/books'
 
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ['author', 'genre', 'introduction', 'publish_year']
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = '/books'
