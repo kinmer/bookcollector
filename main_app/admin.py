bb7a05c5a49c5061acf45ac68ff88672b96240b3
yongZhang <kinmer7@gmail.com>
2024-04-15 20:27:59 -0400

    part 4

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book
+from .models import Book, Reading
+
 admin.site.register(Book)
+admin.site.register(Reading)
--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+
+TIMES = (
+    ('M', 'Morning Reading'), 
+    ('B', 'Bedtime Reading')
+) 
 
 # Create your models here.
 class Book(models.Model):
@@ -14,3 +20,22 @@ class Book(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'book_id': self.id})
+    
+    def read_for_today(self):
+        return self.reading_set.filter(date=date.today()).count() >= len(TIMES)
+
+  
+class Reading(models.Model):
+    date = models.DateField('Reading date')
+    time = models.CharField(
+        max_length=1,
+        choices=TIMES, 
+        default=TIMES[0][0]
+    )
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_time_display()} on {self.date}"
+    class Meta:
+        ordering = ['-date']
+
